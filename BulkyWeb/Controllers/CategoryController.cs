aaaa3ebe966c7b8234115fa320cb67f8a5805252
sreehari <sreehari.n.k18@gmail.com>
2023-09-24 16:11:41 +0530

    second_chage

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) { 
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> category = _db.categories.ToList();
+            return View(category);
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj) { 
+            if(ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           
+            return View();
+        }
+
+    }
+}
---
+++ BulkyWeb/Migrations/20230924082250_SeedcategoryToDb.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedcategoryToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "DisplayOrder", "List", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, false, "Action" },
+                    { 2, 2, false, "SciFi" },
+                    { 3, 3, false, "History" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
