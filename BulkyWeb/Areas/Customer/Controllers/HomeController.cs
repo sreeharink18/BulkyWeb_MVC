e8a94991aa9fe43e1f0debbb03b25ca54c4f97dd
sreehari <sreehari.n.k18@gmail.com>
2023-10-02 07:31:48 +0530

    AddIdentityScafold

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using BulkyWeb.DataAccess.Repository.IRepository;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,17 +9,23 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Product> products = _unitOfWork.Product.GetAll(includeProperties: "category").ToList();
+            return View(products);
         }
-
+        public IActionResult Details(int productId)
+        {
+            Product products = _unitOfWork.Product.Get(u=>u.Id == productId, includeProperties: "category");
+            return View(products);
+        }
         public IActionResult Privacy()
         {
             return View();
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -3,6 +3,7 @@ using BulkyWeb.DataAccess.Data;
 using BulkyWeb.DataAccess.Repository;
 using BulkyWeb.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
 
