427b9128060ca5da3bcaab36de1cbf3ca788cb8d
sreehari <sreehari.n.k18@gmail.com>
2023-10-12 17:37:02 +0530

    toRetreiveorderlist

---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using BulkyWeb.DataAccess.Repository.IRepository;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public OrderController(IUnitOfWork unitOfWork) { 
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		#region Api Call
+		[HttpGet]
+		public IActionResult Get()
+		{
+			List<OrderHeader> orders = _unitOfWork.OrderHeader.GetAll(includeProperties :"ApplicationUser").ToList();
+			return Json(new {data= orders });
+			
+		}
+		#endregion
+	}
+
+}
---
+++ BulkyWeb/wwwroot/js/order.js
@@ -0,0 +1,34 @@
+﻿
+var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    if ($.fn.DataTable.isDataTable('#orderData')) {
+        $('#orderData').DataTable().destroy();
+    }
+    dataTable = $('#orderData').DataTable({
+        "ajax": {url: '/admin/order/get'},
+        "columns": [
+            { data: 'id',"width" :"5%" },
+            { data: 'name', "width": "15%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'applicationUser.email', "width": "15%" },
+            { data: 'orderStatus', "width": "15%" },
+            { data: 'orderTotal', "width": "10%" },
+            
+
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/order/detail?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> </a>               
+                    
+                    </div>`
+                },
+                "width": "25%",
+            },
+        ]
+    });
+}
