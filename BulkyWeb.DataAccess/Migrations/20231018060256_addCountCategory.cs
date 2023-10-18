7b9ef6516f56290b1dea948fb444682d4cd59a91
sreehari <sreehari.n.k18@gmail.com>
2023-10-18 11:35:40 +0530

    AddCountINCategories

---
+++ BulkyWeb.DataAccess/Migrations/20231018060256_addCountCategory.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCountCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CountCategory",
+                table: "categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CountCategory",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CountCategory",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CountCategory",
+                value: 3);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountCategory",
+                table: "categories");
+        }
+    }
+}
